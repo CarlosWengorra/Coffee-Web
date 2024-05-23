0775b1c9bf3636499715c9315f39d4f12e97d2d4
Carlos Wengorra <carloswengorra@outlook.com>
2024-05-23 11:26:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let navbar = document.querySelector(".navbar");
+let searchForm = document.querySelector(".search-form");
+let cartItem = document.querySelector(".cart-items-container");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.toggle("active");
+};
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+};
+
+document.querySelector("#cart-btn").onclick = () => {
+  cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
